7f28827d3083916effb2c37fd60da0afb4f39fd2
Tim120903 <orazowtimurggwp@gmail.com>
2025-12-11 05:13:57 +0300

    Индивидуальное задание

---
+++ individual.py
@@ -0,0 +1,5 @@
+cels = float(input("Please enter any  temperature in celsius:"))
+Farenheit = cels * 1.8 + 32
+Kelvin = cels + 273.15
+print("The temperature in Farenheit is:", Farenheit)
+print("The temperature in Kelvin is:", Kelvin)
